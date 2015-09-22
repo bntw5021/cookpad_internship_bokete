384d3da5eeae1cb8466a810fcb568eca569aefef
d <watanabe1205@gmail.com>
2015-09-22 23:27:20 +0900

    ~p56

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -1,9 +1,17 @@
 class ImagesController < ApplicationController
+
   def index
     @images = Image.all
   end
+
   def new
     @image = Image.new
   end
+
+  def create
+    Image.create(params.require(:image).permit(:url, :title))
+    redirect_to action: :index
+  end
+
 end
 
--- spec/controllers/images_controller_spec.rb
+++ spec/controllers/images_controller_spec.rb
@@ -1,17 +1,30 @@
 require 'rails_helper'
 
 RSpec.describe ImagesController, type: :controller do
+
   describe "#index" do
     it "assign @images" do
       get :index
       expect(assigns(:images)).to_not be_nil
     end
   end
+
   describe "#new" do
     it "assign @image" do
       get :new
       expect(assigns(:image)).to_not be_nil
     end
   end
+
+  describe "#create" do
+    it "create new image" do
+      expect {
+        post :create, image: { url: "http://example.com", title: "test" }
+      }.to change {
+        Image.count
+      }.from(0).to(1)
+    end
+  end
+
 end
 
